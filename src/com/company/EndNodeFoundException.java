94e7fde1b4e46c6f0191d106f6a01bf5cab18d17
Amansinh Parmar <65852341+AmanParmar-git@users.noreply.github.com>
2020-07-24 15:30:16 +0530

    Update EndNodeFoundException.java

--- src/com/company/EndNodeFoundException.java
+++ src/com/company/EndNodeFoundException.java
@@ -5,6 +5,6 @@ import static com.company.Frame.pathFound;
 public class EndNodeFoundException extends Exception{
     @Override
     public void printStackTrace() {
-        System.out.println("Error");
+        System.out.println("Path Found");
     }
 }
